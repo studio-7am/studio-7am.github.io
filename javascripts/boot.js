346364a25d2f6c8548613dd5502c058d72624c16
bj7smth@gmail.com <bj7smth@gmail.com>
2015-07-01 03:56:53 +1000

    Automated commit at 2015-06-30 17:56:53 UTC by middleman-deploy 1.0.0

---
+++ javascripts/boot.js
@@ -0,0 +1,11 @@
+(function() {
+  $(document).ready(function() {
+    return $('html').niceScroll({
+      cursorcolor: '#14AA9F',
+      cursorwidth: "5px"
+    });
+  });
+
+  return;
+
+}).call(this);
